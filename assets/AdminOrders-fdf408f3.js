d0e36b9e913f2fa7fe33a24dbf4ab1f99111a83d
Vera <sk70885@hotmail.com>
2023-03-01 05:04:49 +0800

    deploy

---
+++ assets/AdminOrders-fdf408f3.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as s}from"./index-c33d2dec.js";const o={},r={class:"my-4"};function t(n,_){return c(),s("h2",r,"這是後台訂單頁面")}const d=e(o,[["render",t]]);export{d as default};
---
+++ assets/AdminProducts-77b566fa.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as o}from"./index-c33d2dec.js";const s={},t={class:"my-4"};function n(r,_){return e(),o("h2",t,"這是後台產品頁面")}const d=c(s,[["render",n]]);export{d as default};
