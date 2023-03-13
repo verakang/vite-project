9d566be47432ae9c78acc8b344dd1a87ae494372
Vera <sk70885@hotmail.com>
2023-03-14 02:55:52 +0800

    ProductModal 刪除不要的註解

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -1,24 +1,4 @@
 <template>
-
-  <!-- 勿刪！嘗試正確版 -->
-  <!-- <div class="modal fade" ref="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          嘗試正確版
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
   <div ref="modal" id="productModal" class="modal fade" tabindex="-1">
     <div class="modal-dialog modal-xl">
       <div class="modal-content border-0">
